6c12c414841ea8341bfe715ab4258a08ecb4c92b
KZ211 <alexis.jares12@gmail.com>
2024-02-10 06:15:01 -0300

    Arreglo provisorio para portfolio (El server falla al querer obtener los datos del slug)

--- components/proyecto/proyecto-item.js
+++ components/proyecto/proyecto-item.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function ProyectoItem({ title, slug, image, description}) {
+export default function ProyectoItem({ title, slug, image, description, link}) {
   return (
-    <Link href={`/portfolio/${slug}`}>
+    <Link href={link}>
       <article className='flex flex-col justify-between h-full border-solid overflow-hidden hover:text-white'>
         <header className='flex justify-between'>
             <Image className='flex hover:opacity-10 justify-center absolute w-full h-full rounded-sm' src={image} alt={title} width={1000} height={1000} />
