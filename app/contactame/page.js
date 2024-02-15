29aa280dfb15d993b5999e5fae2fb274f963dc78
KZ211 <alexis.jares12@gmail.com>
2024-02-15 18:43:19 -0300

    Responsive de home

--- app/contactame/page.js
+++ app/contactame/page.js
@@ -4,6 +4,6 @@ export default function ContactamePage(){
     return <header className="flex flex-col justify-center items-center text-center text-white">
         <h1 className="text-center text-4xl font-semibold mt-4 text-white">Redes Sociales</h1>
         <SocialSlideShow/>
-        <p className="text-xl mt-10 backdrop-blur-lg backdrop-filter backdrop-saturate-150 rounded-xl">Muchisimas gracias por ver mi portafolio, me llevo bastante tiempo, <br/> tanto aprender esta tecnologia y desarrollarlo, aun que <br/> lo hice con mucho esmpero y dedicacion😄</p>
+        <p className="text-xl mt-10 backdrop-blur-lg backdrop-filter backdrop-saturate-150 rounded-xl">Muchísimas gracias por ver mi portafolio. Me llevó bastante tiempo<br/> aprender esta tecnología y desarrollarlo, aunque<br/> lo hice con mucho esmero y dedicación. 😄</p>
     </header>
 }
--- app/page.js
+++ app/page.js
@@ -9,31 +9,43 @@ export const metadata = {
 export default function Home() {
   const alexisImage = 'https://drive.google.com/uc?id=1BwVe9HB2NfjvB72hK5tJz2SjxCm6brIm';
   return (
-    <>
-      <header>
-        <div className="flex justify-between items-center">  
+      <header className="flex md:flex-col justify-center items-center">
+        <div className="flex sm:flex-col md:flex-col lg:flex-row justify-between items-center">  
           <Image 
-            className="mt-10 ml-72 rounded-md transition-opacity duration-300 ease-in-out hover:opacity-75"
+            className="
+            sm:mt-3 lg:mt-1 xl:mt-5 2xl:mt-10  
+            lg:ml-32 xl:ml-32 2xl:ml-72 
+            sm:w-52 sm:h-64 md:w-52 md:h-64 lg:size-auto
+            rounded-md transition-opacity duration-300 ease-in-out hover:opacity-75"
             src={alexisImage} 
             alt='Alexis sonriendo con un saco y una remera blanca' 
             width={300} height={300}/>
-            <secction className='text-center mt-14 ml-10 mr-72 text-white'>
-              <h1 className="text-6xl font-exo-2 mb-8">
+            <secction className='
+              sm:mt-1 md:mt-1 lg:mt-3 xl:mt-7 2xl:mt-14 
+              lg:ml-2 xl:ml-5 2xl:ml-10 
+              lg:mr-24 xl:mr-36 2xl:mr-72 
+              text-center  text-white'>
+              <h1 className="
+                sm:text-4xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-6xl
+                sm:mb-1 md:mb-1 lg:mb-2 xl:mb-2 2xl:mb-2
+                font-exo-2">
                 Alexis Isaias Jares
               </h1>
-              <h3 className="mb-5" ><b>Full Stack Developer</b></h3>
-              <p className="text-lg backdrop-filter backdrop-blur-lg backdrop-saturate-100 rounded-xl">Hola, me llamo Alexis Jares, soy estudiante de Ingenieria en Computacion en la Universidad de Tres de Febrero (UNTREF) Y desarrollador web, la tecnologia es mi pasion desde muy pequeño, por eso sacrifique todo para trabajar en el area que tanto me apasiona. <br/> 
+              <h3 className="2xl:mb-3" ><b>Full Stack Developer</b></h3>
+              <p className="
+               sm:text-sm md:text-md lg:text-lg xl:text-lg 2xl:text-lg
+               sm: md:max-w-xl lg:max-w-max
+               backdrop-filter backdrop-blur-lg backdrop-saturate-100 rounded-xl">Hola, me llamo Alexis Jares, soy estudiante de Ingenieria en Computacion en la Universidad de Tres de Febrero (UNTREF) Y desarrollador web, la tecnologia es mi pasion desde muy pequeño, por eso sacrifique todo para trabajar en el area que tanto me apasiona. <br/> 
                 La persona que puso toda su confianza y esmero en verme crecer, la cual me guio y me dio todas las herramientas que necesitaba para formarme hasta llegar a lo que soy, es <a className="text-xl font-bold" href="https://www.linkedin.com/in/stevearagonsite/?originalSubdomain=ar" target="blank" rel="noopener noreferrer">Steven Aragon</a>, 
                 Sr en programacion y licenciado en Desarrollo de Experiencias Virtuales.</p>
-              <div className="flex justify-around mt-8">
-                <Link className="border backdrop-filter backdrop-blur-lg backdrop-saturate-150 rounded-md px-8 py-2 text-xl bg-gradient-to-r from-indigo-700 transition-opacity duration-300 ease-in-out hover:opacity-50" href="/portfolio">Ver Proyectos</Link>
-                <Link className="border backdrop-filter backdrop-blur-lg backdrop-saturate-150 rounded-md px-8 py-2 text-xl transition-opacity duration-300 ease-in-out hover:opacity-50" href="https://docs.google.com/document/d/17VeumtLSh_Ejs1vRHFdE1-_tJW5kojaiL845JQp2W48/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Ver mi CV</Link>
+              <div className="
+                sm: md:mt-1 lg:mt-4 xl:mt-4 2xl:mt-8
+                flex justify-around">
+                <Link className="sm: md:px-6 lg:px-6 xl:px-6 2xl:px-8 sm: md:py-1 lg:py-1 xl:py-1 2xl:py-2 sm: md:text-md lg:text-lg xl:text-lg 2xl:text-xl border backdrop-filter backdrop-blur-lg backdrop-saturate-150 rounded-md bg-gradient-to-r from-indigo-700 transition-opacity duration-300 ease-in-out hover:opacity-50" href="/portfolio">Ver Proyectos</Link>
+                <Link className="sm: md:px-6 lg:px-6 xl:px-6 2xl:px-8 sm: md:py-1 lg:py-1 xl:py-1 2xl:py-2 sm: md:text-md lg:text-lg xl:text-lg 2xl:text-xl border backdrop-filter backdrop-blur-lg backdrop-saturate-150 rounded-md transition-opacity duration-300 ease-in-out hover:opacity-50" href="https://docs.google.com/document/d/17VeumtLSh_Ejs1vRHFdE1-_tJW5kojaiL845JQp2W48/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Ver mi CV</Link>
               </div>
             </secction>
         </div>
       </header>
-      <main>
-      </main>
-    </>
 );
 }
